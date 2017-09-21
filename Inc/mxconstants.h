16c1995270f4c041f7a8f63f3ca962dcee2a31fe
hgaren <g.haddeler@gmail.com>
2017-09-21 17:22:59 +0300

    Inc

---
+++ Inc/mxconstants.h
@@ -0,0 +1,97 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MXCONSTANT_H
+#define __MXCONSTANT_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define IN_S_3_Pin GPIO_PIN_13
+#define IN_S_3_GPIO_Port GPIOC
+#define C_SENSE_Pin GPIO_PIN_14
+#define C_SENSE_GPIO_Port GPIOC
+#define IN_S1_Pin GPIO_PIN_15
+#define IN_S1_GPIO_Port GPIOC
+#define SD_S_Pin GPIO_PIN_0
+#define SD_S_GPIO_Port GPIOA
+#define IN_S_Pin GPIO_PIN_1
+#define IN_S_GPIO_Port GPIOA
+#define IN_S1_5_Pin GPIO_PIN_5
+#define IN_S1_5_GPIO_Port GPIOA
+#define SD_S_5_Pin GPIO_PIN_6
+#define SD_S_5_GPIO_Port GPIOA
+#define IN_S_5_Pin GPIO_PIN_7
+#define IN_S_5_GPIO_Port GPIOA
+#define IN_S1_2_Pin GPIO_PIN_1
+#define IN_S1_2_GPIO_Port GPIOB
+#define SD_S_2_Pin GPIO_PIN_10
+#define SD_S_2_GPIO_Port GPIOB
+#define IN_S_2_Pin GPIO_PIN_11
+#define IN_S_2_GPIO_Port GPIOB
+#define IN_S_1_Pin GPIO_PIN_12
+#define IN_S_1_GPIO_Port GPIOB
+#define SD_S_1_Pin GPIO_PIN_13
+#define SD_S_1_GPIO_Port GPIOB
+#define IN_S1_1_Pin GPIO_PIN_14
+#define IN_S1_1_GPIO_Port GPIOB
+#define IN_S_4_Pin GPIO_PIN_8
+#define IN_S_4_GPIO_Port GPIOA
+#define SD_S_4_Pin GPIO_PIN_11
+#define SD_S_4_GPIO_Port GPIOA
+#define IN_S1_4_Pin GPIO_PIN_12
+#define IN_S1_4_GPIO_Port GPIOA
+#define SD_S_3_Pin GPIO_PIN_7
+#define SD_S_3_GPIO_Port GPIOB
+#define IN_S1_3_Pin GPIO_PIN_8
+#define IN_S1_3_GPIO_Port GPIOB
+#define C_SENSE_3_Pin GPIO_PIN_9
+#define C_SENSE_3_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MXCONSTANT_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
